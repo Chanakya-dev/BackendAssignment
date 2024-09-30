a8d78a360b193b0517a748fbd6ea3ac427ea4e3c
Chanakya <Chanakyamanas123@gmail.com>
2024-09-30 15:50:51 +0530

    Movie

---
+++ src/main/java/Movie/Movie/Controller1.java
@@ -0,0 +1,5 @@
+package Movie.Movie;
+
+public class Controller1 {
+    // Prepare 2 Methods For Get and Post 
+}
---
+++ src/main/java/Movie/Movie/Controller2.java
@@ -0,0 +1,5 @@
+package Movie.Movie;
+
+public class Controller2 {
+    // Prepare a Method for Updating the Data
+}
