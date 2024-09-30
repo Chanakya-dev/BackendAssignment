0cc0e74ed4e0ac3aa6e815596c62e3af446313db
Aparna526 <aparnakondapaturu@gmail.com>
2024-09-30 19:35:38 +0530

    abccd

--- src/main/java/Movie/Movie/Controller1.java
+++ src/main/java/Movie/Movie/Controller1.java
@@ -1,5 +1,24 @@
 package Movie.Movie;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping
 public class Controller1 {
     // Prepare 2 Methods For Get and Post 
+
+    @GetMapping("/getdata")
+    public String getdata(){
+        return "Hello World";
+    }
+
+    @PostMapping("/postdata/{name}")
+    public String postdata(@PathVariable String name){
+        return "Hello "+name;
+    }
 }
--- src/main/java/Movie/Movie/Controller2.java
+++ src/main/java/Movie/Movie/Controller2.java
@@ -1,5 +1,20 @@
 package Movie.Movie;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
 public class Controller2 {
-    // Prepare a Method for Updating the Data
+    private final Service service = new Service();
+
+
+@PostMapping("update")
+public List<MovieApp>update(@RequestBody List<MovieApp>m){
+    return service.update(m);
+}
 }
